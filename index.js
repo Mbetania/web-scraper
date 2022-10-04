b989ba9d5bb30d1b5703cf125bd8884bcc1281df
betaniagonzalez31@gmail.com <betaniagonzalez31@gmail.com>
2022-10-04 10:27:09 -0300

    0223

--- index.js
+++ index.js
@@ -2,35 +2,42 @@ const PORT = 8000
 const axios = require('axios')
 const cheerio = require('cheerio')
 const express = require('express')
-
 const app = express()
+const cors = require('cors')
+app.use(cors())
 
-const url = ('https://www.theguardian.com/uk')
+const url = ('https://www.0223.com.ar/')
 
 //app.METHOD(PATH, HANDLER)
 
 app.get('/', function (req , res){
     res.json('This is my webscaper')
 }) //get data
+
+app.get('/results', (req, res) =>{
+    axios(url)
+        .then(response => {
+            const html = response.data
+            console.log(html)
+            const $ = cheerio.load(html)
+            const articles = []
+            $('.nota__titulo-item', html).each(function() {
+                const title =$(this).text()
+                const url = $(this).find('a').attr('href')
+                articles.push({
+                    title,
+                    url
+                })
+            })
+            // console.log(articles)
+            res.json(articles)
+        }).catch(err => console.log(err))
+
+})
 // app.post() //add
 // app.put() //edit
 // app.delete()
 
-axios(url)
-    .then(response => {
-        const html = response.data
-        const $ = cheerio.load(html)
-        const articles = []
-        $('.fc-item__title', html).each(function() {
-            const title =$(this).text()
-            const url = $(this).find('a').attr('href')
-            articles.push({
-                title,
-                url
-            })
-        })
-        console.log(articles)
-    }).catch(err => console.log(err))
 
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
